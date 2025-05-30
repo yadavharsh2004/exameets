7d0c91b999e2543a0d3f9cf94e942c8660e03187
yadavharsh2004 <harshy8013698@gmail.com>
2025-05-30 12:17:35 +0530

    Add all files

--- src/components/Govt/FilterButton.jsx
+++ src/components/Govt/FilterButton.jsx
@@ -9,8 +9,7 @@ const FilterButton = () => {
       <div className="flex gap-5 justify-between px-7 py-2 rounded-xl bg-white bg-opacity-90 max-md:px-5">
         <span>Filter</span>
         <img
-          // src="https://cdn.builder.io/api/v1/image/assets/TEMP/3a2cb1474046e95ec5a97bfd89d87dc0bde7a322?placeholderIfAbsent=true&apiKey=a2aa714b7b80432f8c  75d4bafeacb991"
-          // alt="Filter icon"
+          alt="Filter icon"
           src= {"/filter.png"}
           className="object-contain shrink-0 my-auto aspect-[1.63] w-[40px]"
         />
--- src/components/Govt/SortButton.jsx
+++ src/components/Govt/SortButton.jsx
@@ -9,7 +9,6 @@ const SortButton = () => {
       <div className="flex gap-5 justify-between px-8 py-1.5 rounded-xl bg-white bg-opacity-90 max-md:px-5">
         <span>Sort by</span>
         <img
-          // src="https://cdn.builder.io/api/v1/image/assets/TEMP/2788c2b0896ec2827d779df74276475e065459bd?placeholderIfAbsent=true&apiKey=a2aa714b7b80432f8c75d4bafeacb991"
           alt="Sort icon"
           src= {"/filter.png"}
           className="object-contain shrink-0 my-auto aspect-[1.63] w-[40px]"
